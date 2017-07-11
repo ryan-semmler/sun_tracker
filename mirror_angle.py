3c0d8d0296eea3c8b6be0ef4d639133575e6c6a7
Ryan Semmler <semmra7@gmail.com>
2017-07-10 23:30:52 -0400

    fixed vertical angle caclulation, added current time feature

--- mirror_angle.py
+++ mirror_angle.py
@@ -1,24 +1,26 @@
 from web_scrape import get_dict
-from math import atan2
+from math import atan2, degrees
+from datetime import datetime as dt
 
 
 def get_date_location():
-    # start_year = input("start year: ")
-    # start_month = input("start month: ")
-    # start_day = input("start day: ")
+    if 'y' in input("Use current date? (y/n) ").lower():
+        now = dt.now()
+        start_year = now.year
+        start_month = now.month
+        start_day = now.day
+    else:
+        start_year = input("start year: ")
+        start_month = input("start month: ")
+        start_day = input("start day: ")
+
+    # currently only supports single-day
     # end_year = input("end year: ")
     # end_month = input("end month: ")
     # end_day = input("end day: ")
+
     # latitude = input("latitude: ")
     # longitude = input("longitude: ")
-    start_year = '2017'
-    start_month = '7'
-    start_day = '9'
-
-    # currently only supports single-day
-    # end_year = '2017'
-    # end_month = '6'
-    # end_day = '27'
 
     latitude = '35.951053'
     longitude = '-78.544401'
@@ -35,10 +37,10 @@ def vert_angle(time):
     h = 8
     d = 5
 
-    return (atan2(h, d) - data[time]['altitude']) / 2
+    return (degrees(atan2(h, d)) - data[time]['altitude']) / 2
 
 
 for i in range(6, 19):
     time = '{}:00:00'.format(i)
-    time_data = data[time]
+    # time_data = data[time]
     print(time + ':', 'altitude:', data[time]['altitude'], 'd:', vert_angle(time))
